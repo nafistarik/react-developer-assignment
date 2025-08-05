0cee38f400e5e75278b77f3d041c4090029bae10
nafistarik <nafistarik789@gmail.com>
2025-08-06 01:25:23 +0600

    enhance: reset button added

--- src/features/tic-tac-toe/components/board.tsx
+++ src/features/tic-tac-toe/components/board.tsx
@@ -4,7 +4,9 @@ import Square from "./square";
 import { calculateWinner } from "../utils/calculate-winner";
 
 export default function Board() {
-  const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null));
+  const [squares, setSquares] = useState<(string | null)[]>(
+    Array(9).fill(null)
+  );
   const [isFirstPlayerTurn, setIsFirstPlayerTurn] = useState(true);
   const winner = calculateWinner(squares);
   const isBoardFull = squares.every(Boolean);
@@ -29,16 +31,25 @@ export default function Board() {
     <div className="flex flex-col gap-4 px-4 py-6 border border-muted rounded-lg">
       <h1 className="text-primary font-bold text-2xl">{status}</h1>
       <div className="grid grid-cols-3 gap-3 w-64 h-64">
-        {squares.map((value, index) => {
-          return (
-            <Square
-              key={index}
-              value={value}
-              onClick={() => handleSquareClick(index)}
-            />
-          );
-        })}
+        {squares.map((value, index) => (
+          <Square
+            key={index}
+            value={value}
+            onClick={() => handleSquareClick(index)}
+          />
+        ))}
       </div>
+      {winner || isBoardFull ? (
+        <button
+          className="mt-4 px-4 py-2 bg-primary text-white rounded-radius hover:bg-primary-hover hover:cursor-pointer transition-base"
+          onClick={() => {
+            setSquares(Array(9).fill(null));
+            setIsFirstPlayerTurn(true);
+          }}
+        >
+          Play Again
+        </button>
+      ) : null}
     </div>
   );
 }
