b69ddcf16875a8becbbbbecd1f6bf56e5085ad47
nafistarik <nafistarik789@gmail.com>
2025-08-06 01:12:22 +0600

    feat: winner calculation is done

--- src/features/tic-tac-toe/components/board.tsx
+++ src/features/tic-tac-toe/components/board.tsx
@@ -1,27 +1,30 @@
 "use client";
 import { useState } from "react";
 import Square from "./square";
+import { calculateWinner } from "../utils/calculate-winner";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isFirstPlayerTurn, setIsFirstPlayerTurn] = useState(true);
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? winner === "X"
+      ? "1st Player Wins! 🎉"
+      : "2nd Player Wins! 🎉"
+    : isFirstPlayerTurn
+    ? "1st Player turn:"
+    : "2nd Player turn:";
   const handleSquareClick = (index: number) => {
     if (squares[index]) return;
     const newSquares = [...squares];
-    if (isFirstPlayerTurn) {
-      newSquares[index] = "X";
-    } else {
-      newSquares[index] = "O";
-    }
+    newSquares[index] = isFirstPlayerTurn ? "X" : "O";
     setSquares(newSquares);
     setIsFirstPlayerTurn(!isFirstPlayerTurn);
   };
 
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-primary font-bold text-2xl">
-        {isFirstPlayerTurn ? "1st " : "2nd "}Player turn:
-      </h1>
+    <div className="flex flex-col gap-4 px-4 py-6 border border-muted rounded-lg">
+      <h1 className="text-primary font-bold text-2xl">{status}</h1>
       <div className="grid grid-cols-3 gap-3 w-64 h-64">
         {squares.map((value, index) => {
           return (
--- src/features/tic-tac-toe/components/square.tsx
+++ src/features/tic-tac-toe/components/square.tsx
@@ -3,7 +3,7 @@ import { SquareType } from "../types/game-types";
 export default function Square({ value, onClick }: SquareType) {
   return (
     <button
-      className="aspect-square w-20 flex items-center justify-center text-2xl font-bold bg-card text-primary
+      className="aspect-square flex items-center justify-center text-2xl font-bold bg-card text-primary
         border-2 border-border rounded-radius hover:bg-transparent hover:cursor-pointer transition-base"
       onClick={onClick}
     >
