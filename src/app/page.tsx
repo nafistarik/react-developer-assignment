89d951cc9fcafe70004ee872ad9113c7055a120d
nafistarik <nafistarik789@gmail.com>
2025-08-05 19:44:15 +0600

    enhance: global css setup with dark mode variables also syncing layout and main UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="text-3xl font-bold flex items-center justify-center min-h-[100vh]">
+    <div className="text-3xl text-primary font-bold flex items-center justify-center min-h-[100vh]">
       React Developer Assignment
     </div>
   );
